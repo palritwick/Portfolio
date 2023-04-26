ff3e5a60e0d4d7183b73ede212fd30a73533fbc3
Ritwick Pal <108950247+palritwick@users.noreply.github.com>
2023-04-26 16:00:56 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,80 @@
+// Responsible navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+// Scroll section active link
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header navbar a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header navbar a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    // sticky behaviour
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // Remove toggle and navbar when click on navbar item
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+// scroll reveal
+ScrollReveal(
+    {
+        // reset: true,
+        distance: '80px',
+        duration: 2000,
+        delay: 200
+    });
+
+ScrollReveal().reveal('.home-content,.heading', { origin: 'top' });
+ScrollReveal().reveal('.home-image, .skills-container,.portfolio-box,.contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1,.about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content p, tab-titles', { origin: 'right' });
+
+// Typed js
+const typed = new Typed('.multi-text', {
+    strings: ['Frontend Developer', 'Web Designer', 'Programmer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-170px";
+}
